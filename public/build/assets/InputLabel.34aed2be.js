98d899c853400255bfb2c499237eb456ad22fcdc
ulises-mendez <memorandum06@gmail.com>
2022-09-26 17:25:27 -0500

    build push

--- public/build/assets/InputLabel.34aed2be.js
+++
@@ -1,1 +0,0 @@
-import{R as r}from"./app.db968ba3.js";function l({forInput:e,value:t,className:a,children:m}){return r.createElement("label",{htmlFor:e,className:"block font-medium text-sm text-gray-700 "+a},t||m)}export{l as I};
