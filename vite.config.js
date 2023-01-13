8466a1809e5b3f148bb80dba19b9ef7d472f28ff
ulises-mendez <memorandum06@gmail.com>
2023-01-12 20:07:07 -0600

    release

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    /*
+  /*
     server: {
         origin: 'http://127.0.0.1:8001',
         port: 3001,
       },
     */
+      define: {
+        "global": {},
+      },
       plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             //input: ['resources/css/app.css', 'resources/js/app.js'],
-            //refresh: true,
+            refresh: true,
         }),
     ],
 });
